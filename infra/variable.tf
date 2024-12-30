8518559c1868139558bb6a2e82475ba9b4e5315e
ryosuke-horie <r_horie@sincereed.com>
2024-12-30 19:18:23 +0900

    Terraform initが実施できるように調整

---
+++ infra/variable.tf
@@ -0,0 +1,10 @@
+variable "project_id" {
+  description = "project_id"
+  type        = string
+  default     = "plasma-renderer-446307-u5"
+}
+
+variable "default_region" {
+  description = "The default region for resources"
+  default     = "asia-southeast1"
+}
