d18d9c821625ace34a964cab66502c71340390dc
ryosuke-horie <r_horie@sincereed.com>
2024-12-31 18:54:47 +0900

    CORSのドメイン制御を全許可

--- backend/middleware/cors.go
+++ backend/middleware/cors.go
@@ -8,7 +8,7 @@ import (
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// 必要に応じてオリジンを動的に設定
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
